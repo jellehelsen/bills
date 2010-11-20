5a4cb9b5427c22fd721147aa45f98115b3628f8f
Jelle Helsen <jelle.helsen@hcode.be>
2010-11-20 10:28:48 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bills::Application.config.secret_token = '367653dd2e819d4338a288fd1dd88db8ad1fd529c1c3d1739b7eadca4b0e68bc7a3966e38f6ac0a4c0f8b8d1c7e0f96526305236726d8d11a60d75006d3934a8'
