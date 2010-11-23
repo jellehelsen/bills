44a9da0e58b21bf5a8a86fe22ba40cb80ee2f701
Jelle Helsen <jelle.helsen@hcode.be>
2010-11-23 16:26:52 +0100

    fixing problem with date parser

--- config/initializers/validates_timeliness.rb
+++ config/initializers/validates_timeliness.rb
@@ -3,5 +3,6 @@
     # Extend ORM/ODMs for full support (:active_record, :mongoid).
     config.extend_orms = [ :mongoid ]
     config.use_plugin_parser = true
+    Timeliness.use_euro_formats
   end
 
