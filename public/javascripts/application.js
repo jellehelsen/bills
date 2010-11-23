e65668a72c07ab5de8b844bbb923b44aa2be5f23
Jelle Helsen <jelle.helsen@hcode.be>
2010-11-23 11:05:53 +0100

    added datepicker

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $.datepicker.setDefaults( $.datepicker.regional[ "nl" ] );
+  $( "#bill_due_date" ).datepicker( $.datepicker.regional[ "nl" ] );
+});
